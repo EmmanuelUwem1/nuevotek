8c49503c6e26861c3c60373757159ec00410e621
Emmanuel Uwem <emmanueluwem560@gmail.com>
2024-12-13 19:05:51 +0100

    updated: style

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,14 +6,14 @@ function Footer() {
     <footer className="flex flex-col justify-star w-full px-4 sm:px-8 md:px-20">
       <div className="bg-[#D8D8D8] w-full h-[2px]"></div>
       <div className="flex justify-between items-center gap-4 h-16">
-        <p className="font-styrene font-medium text-[0.6rem] md:text-base">
+        <p className="font-styrene font-medium text-[0.6rem] md:text-[0.8rem]">
           Copyright © 2024 Neuvotech. All Rights Reserved
         </p>
         <div className="flex justify-between items-center gap-7">
-          <Link href={"/"} className="font-styrene font-medium text-[0.6rem] md:text-base">
+          <Link href={"/"} className="font-styrene font-medium text-[0.6rem] md:text-[0.8rem]">
             Privacy Policy
           </Link>
-          <Link href={"/"} className="font-styrene font-medium text-[0.6rem] md:text-base">
+          <Link href={"/"} className="font-styrene font-medium text-[0.6rem] md:text-[0.8rem]">
             Terms of Service
           </Link>
         </div>
