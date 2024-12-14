6ae61b1e11c23117455a100f9fc8bb211c9d92c8
Emmanuel Uwem <emmanueluwem560@gmail.com>
2024-12-14 17:21:57 +0100

    feat: updated style

--- src/app/contact/components/getInTouch.jsx
+++ src/app/contact/components/getInTouch.jsx
@@ -3,7 +3,7 @@ import Form from "../form";
 function GetInTouch() {
   return (
     <section className="flex-col items-center justify-center flex w-full py-16">
-      <div className="flex flex-col w-1/2 md:w-2/3 justify-center items-center h-full">
+      <div className="flex flex-col w-full md:w-2/3 justify-center items-center h-full">
         <h2 className="font-medium text-4xl lg:text-6xl text-[var(--primary-color)] text-center">
           Get in touch
         </h2>
