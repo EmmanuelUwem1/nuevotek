d0de7870c439db72e1d45a5c0e323d59f6e3e20b
Emmanuel Uwem <emmanueluwem560@gmail.com>
2024-12-19 15:03:13 +0100

    Update2 form.jsx

--- src/app/contact/form.jsx
+++ src/app/contact/form.jsx
@@ -1,11 +1,8 @@
 "use client";
 import React, { useRef, useState } from "react";
-
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import emailjs from "@emailjs/browser";
-
-// emailjs.init(process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY);
 
 function PhoneNumberInput({ value, onChange }) {
   return (
@@ -63,9 +60,17 @@ function Form() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // const serviceID = process.env.local.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
-    // const templateID = process.env.local.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
-    // const userID = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY;
+    // Validation check
+    if (
+      !userInput.name ||
+      !userInput.company_name ||
+      !userInput.email ||
+      !userInput.message ||
+      !userInput.number
+    ) {
+      toast.error("Please fill in all fields.");
+      return;
+    }
 
     try {
       const emailParams = {
@@ -90,7 +95,7 @@ function Form() {
           name: "",
           email: "",
           number: "",
-          company_name:"",
+          company_name: "",
           message: "",
         });
       }
@@ -101,69 +106,68 @@ function Form() {
 
   const form = useRef();
 
-
-    return (
-      <>
-        <form
-          ref={form}
-          onSubmit={handleSubmit}
-          className="flex justify-normal flex-col items-center w-full gap-4 pr-4 mt-8"
+  return (
+    <>
+      <form
+        ref={form}
+        onSubmit={handleSubmit}
+        className="flex justify-normal flex-col items-center w-full gap-4 pr-4 mt-8"
+      >
+        <div className="flex flex-wrap md:flex-nowrap w-full gap-4">
+          <input
+            type="text"
+            name="name"
+            onChange={handleChange}
+            value={userInput.name}
+            id="name"
+            placeholder="Enter your full name"
+            className="bg-[#F4F4F4] h-16 flex w-full rounded-sm px-4 focus:border-[1px] focus:border-[var(--primary-color)]"
+          />
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={userInput.email}
+            onChange={handleChange}
+            placeholder="Enter your E-mail"
+            className="bg-[#F4F4F4] h-16 flex w-full rounded-sm px-4 focus:border-[1px] focus:border-[var(--primary-color)]"
+          />
+        </div>
+        <div className="flex flex-wrap md:flex-nowrap w-full gap-4">
+          <PhoneNumberInput
+            value={userInput.number}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            onChange={handleChange}
+            name="company_name"
+            value={userInput.company_name}
+            id="company_name"
+            placeholder="Enter your company’s name"
+            className="bg-[#F4F4F4] h-16 flex w-full rounded-sm px-4 focus:border-[1px] focus:border-[var(--primary-color)]"
+          />
+        </div>
+        <div className="flex w-full">
+          <textarea
+            name="message"
+            id="message"
+            value={userInput.message}
+            onChange={handleChange}
+            placeholder="Your message here"
+            className="bg-[#F4F4F4] h-72 w-full rounded-sm p-4 focus:border-[1px] focus:border-[var(--primary-color)]"
+          ></textarea>
+        </div>
+        <button
+          name="user_first_name"
+          className="font-styrene flex w-full justify-center items-center font-medium text-base bg-[var(--primary-color)] text-[#FFFFFF] px-8 py-3 transition-all hover:bg-black"
         >
-          <div className="flex flex-wrap md:flex-nowrap w-full gap-4">
-            <input
-              type="text"
-              name="name"
-              onChange={handleChange}
-              value={userInput.name}
-              id="name"
-              placeholder="Enter your full name"
-              className="bg-[#F4F4F4] h-16 flex w-full rounded-sm px-4 focus:border-[1px] focus:border-[var(--primary-color)]"
-            />
-            <input
-              type="email"
-              name="email"
-              id="email"
-              value={userInput.email}
-              onChange={handleChange}
-              placeholder="Enter your E-mail"
-              className="bg-[#F4F4F4] h-16 flex w-full rounded-sm px-4 focus:border-[1px] focus:border-[var(--primary-color)]"
-            />
-          </div>
-          <div className="flex flex-wrap md:flex-nowrap w-full gap-4">
-            <PhoneNumberInput
-              value={userInput.number}
-              onChange={handleChange}
-            />
-            <input
-              type="text"
-              onChange={handleChange}
-              name="company_name"
-              value={userInput.company_name}
-              id="company_name"
-              placeholder="Enter your company’s name"
-              className="bg-[#F4F4F4] h-16 flex w-full rounded-sm px-4 focus:border-[1px] focus:border-[var(--primary-color)]"
-            />
-          </div>
-          <div className="flex w-full">
-            <textarea
-              name="message"
-              id="message"
-              value={userInput.message}
-              onChange={handleChange}
-              placeholder="Your message here"
-              className="bg-[#F4F4F4] h-72 w-full rounded-sm p-4 focus:border-[1px] focus:border-[var(--primary-color)]"
-            ></textarea>
-          </div>
-          <button
-            name="user_first_name"
-            className="font-styrene flex w-full justify-center items-center font-medium text-base bg-[var(--primary-color)] text-[#FFFFFF] px-8 py-3 transition-all hover:bg-black"
-          >
-            Send a Message
-          </button>
-        </form>
-        <ToastContainer />
-      </>
-    );
+          Send a Message
+        </button>
+      </form>
+      <ToastContainer />
+    </>
+  );
 }
 
-export default Form
+export default Form;
